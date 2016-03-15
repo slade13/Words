870ccbaf4fc98e12a5912e5dfab63e5f439ae64a
Mateusz Lubiński <mateuszlubinski@hotmail.com>
2016-03-15 16:43:37 +0100

    fix for seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,26 +8,26 @@
 
 c1 = Category.create(name: 'Basics', description: 'Elementary words.', image: 'http://orig00.deviantart.net/07e2/f/2011/035/a/1/simplicity_is_beauty_by_cifro-d38rys2.png');
 
-p1 = Package.create(name: 'Colors', description: 'Learn colors in easy way!', category: 1);
+p1 = Package.create(name: 'Colors', description: 'Learn colors in easy way!', category: c1);
 
-Word.create(english_name: 'red', polish_name: 'czerwony', package: 1);
-Word.create(english_name: 'green', polish_name: 'zielony', package: 1);
-Word.create(english_name: 'blue', polish_name: 'niebieski', package: 1);
-Word.create(english_name: 'cyan', polish_name: 'akwamarynowy', package: 1);
-Word.create(english_name: 'yellow', polish_name: 'żółty', package: 1);
-Word.create(english_name: 'pink', polish_name: 'różowy', package: 1);
-Word.create(english_name: 'black', polish_name: 'czarny', package: 1);
-Word.create(english_name: 'white', polish_name: 'biały', package: 1);
-Word.create(english_name: 'silver', polish_name: 'srebrny', package: 1);
+Word.create(english_name: 'red', polish_name: 'czerwony', package: p1);
+Word.create(english_name: 'green', polish_name: 'zielony', package: p1);
+Word.create(english_name: 'blue', polish_name: 'niebieski', package: p1);
+Word.create(english_name: 'cyan', polish_name: 'akwamarynowy', package: p1);
+Word.create(english_name: 'yellow', polish_name: 'żółty', package: p1);
+Word.create(english_name: 'pink', polish_name: 'różowy', package: p1);
+Word.create(english_name: 'black', polish_name: 'czarny', package: p1);
+Word.create(english_name: 'white', polish_name: 'biały', package: p1);
+Word.create(english_name: 'silver', polish_name: 'srebrny', package: p1);
 
-p2 = Package.create(name: 'Home', description: 'Types of buildings and furnitures', category: 1);
+p2 = Package.create(name: 'Home', description: 'Types of buildings and furnitures', category: c1);
 
-Word.create(english_name: 'home', polish_name: 'dom', package: 2);
-Word.create(english_name: 'table', polish_name: 'stół', package: 2);
-Word.create(english_name: 'desk', polish_name: 'biórko', package: 2);
-Word.create(english_name: 'flat', polish_name: 'mieszkanie', package: 2);
-Word.create(english_name: 'kitchen', polish_name: 'kuchnia', package: 2);
-Word.create(english_name: 'room', polish_name: 'pokój', package: 2);
+Word.create(english_name: 'home', polish_name: 'dom', package: p2);
+Word.create(english_name: 'table', polish_name: 'stół', package: p2);
+Word.create(english_name: 'desk', polish_name: 'biórko', package: p2);
+Word.create(english_name: 'flat', polish_name: 'mieszkanie', package: p2);
+Word.create(english_name: 'kitchen', polish_name: 'kuchnia', package: p2);
+Word.create(english_name: 'room', polish_name: 'pokój', package: p2);
 
 c2 = Category.create(name: 'Phrases', description: 'More complicated sets of words.', image: 'http://thedeconstruction.org/wp-content/uploads/2014/11/words1.jpg');
 c3 = Category.create(name: 'People', description: 'Everything connected with human living.', image: 'http://img0.gtsstatic.com/wallpapers/ccacc01a5c78f9269a8c9298796e9867_large.jpeg');
